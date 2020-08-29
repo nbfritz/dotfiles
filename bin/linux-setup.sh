6325e4153f8b0953352708b829892ff9d25d2c81
Nathan Fritz <nbfritz@gmail.com>
2020-08-28 19:45:12 -0500

    Add linux setup script

---
+++ bin/linux-setup.sh
@@ -0,0 +1,63 @@
+# Setup for Debian WSL
+
+echo "********************************************************************************"
+echo "  Updating Linux Packages"
+echo "********************************************************************************"
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y vim tmux git zsh curl
+
+
+echo
+echo "********************************************************************************"
+echo "  Installing OhMyZsh"
+echo "********************************************************************************"
+
+if [ ! -d ~/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo "SKIPPING..."
+fi
+
+
+echo
+echo "********************************************************************************"
+echo "  Installing ASDF"
+echo "********************************************************************************"
+  
+if [ ! -d ~/.asdf ]; then
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0-rc1
+else
+  echo "SKIPPING..."
+fi
+
+
+echo
+echo "********************************************************************************"
+echo "  Creating SSH Identity"
+echo "********************************************************************************"
+  
+if [ ! -f ~/.ssh/id_rsa ]; then
+  ssh-keygen
+  cat ~/.ssh/id_rsa.pub | /mnt/c/Windows/system32/clip.exe
+  echo "The clipboard now contains the public key. Paste it into github!"
+  echo "Press [ENTER] to continue..."
+  read _
+else
+  echo "SKIPPING..."
+fi
+
+
+echo
+echo "********************************************************************************"
+echo "  Installing dotfiles"
+echo "********************************************************************************"
+
+  
+if [ ! -f ~/.dotfiles ]; then
+  git clone git@github.com:nbfritz/dotfiles ~/.dotfiles
+  sh ~/.dotfiles/create-dotfiles-symlinks.sh
+else
+  echo "SKIPPING..."
+fi
